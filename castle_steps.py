1a0cbb952fd92b9029bf50d541bdec0f9bfa7a16
Katalina Samarina <99908400+spiritiez@users.noreply.github.com>
2023-03-16 15:58:54 +0800

    Update castle_steps.py

--- castle_steps.py
+++ castle_steps.py
@@ -4,6 +4,7 @@ with open("/Users/Ekaterina/Desktop/mypr/castle.json") as data:
     
     castle_quest_data = list(json.load(data))
     print(castle_quest_data[0])
+    print()
     print('Your actions:\n' + '\n'.join(list(castle_quest_data[1].keys())))
     your_step = castle_quest_data[1]
     
@@ -15,13 +16,16 @@ with open("/Users/Ekaterina/Desktop/mypr/castle.json") as data:
         if your_step == "return to enter":
             your_step = castle_quest_data[1]
             print(castle_quest_data[0])
+            print()
             print('Your actions:\n' + '\n'.join(list(your_step[1].keys())))
             continue
             
         if type(your_step) == str:
+            print()
             print(your_step)
             break
             
         print(your_step[0])
+        print()
         print('Your actions:\n' + '\n'.join(list(your_step[1].keys())))
         your_step = your_step[1]  
